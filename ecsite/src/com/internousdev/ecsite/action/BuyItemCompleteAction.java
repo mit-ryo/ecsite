2ef7a297fc5da94ac96adba7ac78cc4bb133b715
Home <kiraboshiizm8@hotmail.com>
2019-02-06 02:15:08 +0900

    19/02/06

---
+++ ecsite/src/com/internousdev/ecsite/action/BuyItemCompleteAction.java
@@ -0,0 +1,28 @@
+package com.internousdev.ecsite.action;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuyItemCompleteAction extends ActionSupport implements SessionAware{
+
+	private Map<String,Object> session;
+
+	public String execute()throws SQLException{
+
+		String result =ERROR;
+		return result;
+	}
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session){
+		this.session = session;
+	}
+}
